209af6d2a49773a9eeaf5cc95701acb3ea321e87
dominique youmans and nikhil batta <dominique@>
2019-08-14 14:11:45 -0700

    adds functionality for animation slideins and toggling stuff

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,25 @@ $(document).ready(function(){
   $("form#appointments").submit(function(event){
     event.preventDefault()
     var result = $("#born").val();
-    console.log(result);
-    $("#text1").text(result);
-    $(".hide").toggle();
+    if (!result){
+      $(".hide3").show();
+    } else {
+      $(".hide3").hide();
+      $("#text1").text(result);
+      $(".hide").show();
+    }
+  });
+  $(".form-control").submit(function(event){
+    var beverage = $("#beverage").val();
+    
   })
 
-})
+
+  $("#stack").click(function(){
+    $("#lightbulb").addClass("fa-lightbulb");
+  })
+ //$("#stack").click(function(){
+ //
+ //   $("#hide2").toggle();
+//});
+});
